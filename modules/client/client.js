590517c037893f1199b515c149eac89c9285dec0
lheido <leonard.treille@gmail.com>
2017-02-06 22:51:33 +0100

    init polymer client

--- modules/client/client.js
+++ modules/client/client.js
@@ -7,10 +7,10 @@ var static_dirs = [
   '/app/',
 ];
 for (var i in static_dirs) {
-  app.use(express.static(__dirname+static_dirs[i]));
+  app.use(express.static(__dirname + static_dirs[i]));
 }
 
-app.get('/', function(req, res){
-  logger.debug('home request');
-  res.sendFile(__dirname + '/client/app.html');
-});
+// app.get('/', function(req, res){
+//   logger.debug('home request');
+//   res.sendFile(__dirname + '/app/index.html');
+// });
