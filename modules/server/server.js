cce25e5ddc6db30688b23a2947f63b3fff127506
lheido <leonard.treille@gmail.com>
2017-02-11 16:48:28 +0100

    add local network ip log info

--- modules/server/server.js
+++ modules/server/server.js
@@ -1,3 +1,5 @@
+var os      = require('os');
+var ifaces  = os.networkInterfaces();
 var program = require('commander');
 var logger  = require('node-yolog');
 var express = require('express');
@@ -11,6 +13,24 @@ server.listen(PORT, HOSTNAME, null, function() {
   let address = server.address();
   let path = address.address;
   logger.info('Application runs: http://' + path + ':' + PORT);
+  logger.info('Check available ip to test with device on the same network:');
+  Object.keys(ifaces).forEach(function (ifname) {
+    var alias = 0;
+    ifaces[ifname].forEach(function (iface) {
+      if ('IPv4' !== iface.family || iface.internal !== false) {
+        // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+        return;
+      }
+      if (alias >= 1) {
+        // this single interface has multiple ipv4 addresses
+        logger.info('\t-', ifname + ':' + alias, iface.address);
+      } else {
+        // this interface has only one ipv4 adress
+        logger.info('\t-', ifname, iface.address);
+      }
+      ++alias;
+    });
+  });
 });
 
 
