bbc366f98e2cda0416012f8e81054a5bce2936c1
lheido <leonard.treille@gmail.com>
2017-02-13 00:48:11 +0100

    rewrite server.js with ts syntax

--- src/modules/server/server.js
+++ src/modules/server/server.js
@@ -1,36 +1,34 @@
-var os = require('os');
-var ifaces = os.networkInterfaces();
-var program = require('commander');
-var logger = require('node-yolog');
-var express = require('express');
-var app = express();
-var server = require('http').Server(app);
-var io = require('socket.io').listen(server);
-let PORT = program.port || 3000;
-let HOSTNAME = program.hostname || '0.0.0.0';
-server.listen(PORT, HOSTNAME, null, function () {
-    let address = server.address();
+"use strict";
+const os = require("os");
+const program = require("commander");
+const logger = require("node-yolog");
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+exports.app = express();
+exports.server = http.createServer(exports.app);
+exports.io = socketIo.listen(exports.server);
+let ifaces = os.networkInterfaces();
+const PORT = program.port || 3000;
+const HOSTNAME = program.hostname || "0.0.0.0";
+exports.server.listen(PORT, HOSTNAME, null, function () {
+    let address = exports.server.address();
     let path = address.address;
-    logger.info('Application runs: http://' + path + ':' + PORT);
-    logger.info('Check available ip to test with device on the same network:');
+    logger.info("Application runs: http://" + path + ":" + PORT);
+    logger.info("Check available ip to test with device on the same network:");
     Object.keys(ifaces).forEach(function (ifname) {
-        var alias = 0;
+        let alias = 0;
         ifaces[ifname].forEach(function (iface) {
-            if ('IPv4' !== iface.family || iface.internal !== false) {
+            if ("IPv4" !== iface.family || iface.internal !== false) {
                 return;
             }
             if (alias >= 1) {
-                logger.info('\t-', ifname + ':' + alias, iface.address);
+                logger.info("\t-", ifname + ":" + alias, iface.address);
             }
             else {
-                logger.info('\t-', ifname, iface.address);
+                logger.info("\t-", ifname, iface.address);
             }
             ++alias;
         });
     });
 });
-module.exports = {
-    'app': app,
-    'io': io,
-    'server': server
-};
--- src/modules/server/server.ts
+++ src/modules/server/server.ts
@@ -1,42 +1,38 @@
-var os      = require('os');
-var ifaces  = os.networkInterfaces();
-var program = require('commander');
-var logger  = require('node-yolog');
-var express = require('express');
-var app     = express();
-var server  = require('http').Server(app);
-var io      = require('socket.io').listen(server);
+import * as os from "os";
+import * as program from "commander";
+import * as logger from "node-yolog";
+import * as express from "express";
+import * as http from "http";
+import * as socketIo from "socket.io";
 
-let PORT = program.port || 3000;
-let HOSTNAME = program.hostname || '0.0.0.0';
+export let app = express();
+export let server = http.createServer(app);
+export let io = socketIo.listen(server);
+
+let ifaces  = os.networkInterfaces();
+
+const PORT = program.port || 3000;
+const HOSTNAME = program.hostname || "0.0.0.0";
 server.listen(PORT, HOSTNAME, null, function() {
   let address = server.address();
   let path = address.address;
-  logger.info('Application runs: http://' + path + ':' + PORT);
-  logger.info('Check available ip to test with device on the same network:');
+  logger.info("Application runs: http://" + path + ":" + PORT);
+  logger.info("Check available ip to test with device on the same network:");
   Object.keys(ifaces).forEach(function (ifname) {
-    var alias = 0;
+    let alias = 0;
     ifaces[ifname].forEach(function (iface) {
-      if ('IPv4' !== iface.family || iface.internal !== false) {
+      if ("IPv4" !== iface.family || iface.internal !== false) {
         // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
         return;
       }
       if (alias >= 1) {
         // this single interface has multiple ipv4 addresses
-        logger.info('\t-', ifname + ':' + alias, iface.address);
+        logger.info("\t-", ifname + ":" + alias, iface.address);
       } else {
         // this interface has only one ipv4 adress
-        logger.info('\t-', ifname, iface.address);
+        logger.info("\t-", ifname, iface.address);
       }
       ++alias;
     });
   });
 });
-
-
-
-module.exports = {
-  'app': app,
-  'io': io,
-  'server': server
-}
